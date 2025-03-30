c5259500faf1b029056a6693f7809c3d24bed90c
ramcan7 <m.ramos@pucp.edu.pe>
2025-03-30 16:43:30 -0500

    Call to PokéApi implemented.
    151 Pokemon List now functional with every Pokemon having their respective info.

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const pokemonList = document.querySelector("#pokemonList");
+let URL = "https://pokeapi.co/api/v2/pokemon/";
+for(let i = 1; i <= 151; i++) {
+    fetch(URL + i)
+        .then(response => response.json())
+        .then(data => showPokemon(data))
+}
+
+function showPokemon(pokemon) {
+    let types = pokemon.types.map((type) => `<p class="${type.type.name} type">${type.type.name}</p>`);
+    types = types.join('');
+
+    let pokemonId = pokemon.id.toString();
+    pokemonId = pokemonId.padStart(3, "0")
+
+    const div = document.createElement("div");
+    div.classList.add("pokemon");
+    div.innerHTML = `<p class="pokemon-id-back">#${pokemonId}</p>
+                    <div class="pokemon-image">
+                        <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}">
+                    </div>
+                    <div class="pokemon-info">
+                        <div class="name-container">
+                            <p class="pokemon-id">
+                                ${pokemonId}
+                            </p>
+                            <h2 class="pokemon-name">
+                                ${pokemon.name}
+                            </h2>
+                        </div>
+                        <div class="pokemon-types">
+                            ${types}
+                        </div>
+                        <div class="pokemon-stats">
+                            <p class="stat">${pokemon.height}m</p>
+                            <p class="stat">${pokemon.weight}kg</p>
+                        </div>
+                    </div>`;
+    pokemonList.append(div);
+}
